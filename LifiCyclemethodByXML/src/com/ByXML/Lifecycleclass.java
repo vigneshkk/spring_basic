e48f580fcbaf4bd94bee854cb79b075cc5f7b91c
vignesh <vignesh.it16@bitsathy.ac.in>
2021-04-05 19:02:57 +0530

    Add files via upload

---
+++ LifiCyclemethodByXML/src/com/ByXML/Lifecycleclass.java
@@ -0,0 +1,13 @@
+package com.ByXML;
+
+public class Lifecycleclass {
+
+	public void myInitmethod() throws Exception {
+		System.out.println("Init method");
+	}
+
+	
+	public void myDestroymethod() throws Exception {
+		System.out.println("destroy method");
+	}
+}
---
+++ LifiCyclemethodByXML/src/com/ByXML/Lifecycleclass1.java
@@ -0,0 +1,14 @@
+package com.ByXML;
+
+public class Lifecycleclass1 {
+
+	public void myInitmethod1() throws Exception {
+		System.out.println("Init method");
+	}
+
+	
+	public void myDestroymethod1() throws Exception {
+		System.out.println("destroy method");
+	}
+	
+}
